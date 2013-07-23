594213af26ed704bccd8ab272c2b5d6790d39be4
David Somers <jalada@gmail.com>
2013-07-23 21:44:39 +0100

    Tweaks, better splitting of data.

--- publication.rb
+++ publication.rb
@@ -15,7 +15,7 @@ feed_url = "http://www.spanishdict.com/wordoftheday/feed"
 # HTML/CSS edition with etag. This publication changes the greeting depending
 # on the time of day. It is using UTC to determine the greeting.
 #
-get '/edition/' do
+get '/edition/?' do
 
   response = Typhoeus::Request.get feed_url
   if response.success?
@@ -25,7 +25,11 @@ get '/edition/' do
     pieces_of_title = latest_word.at_css("title").text.split(" - ")
     @word = pieces_of_title[1]
     @definition = pieces_of_title[2]
-    @example = Sanitize.clean(latest_word.at_css("description").text)
+    other = Sanitize.clean(latest_word.at_css("description").text)
+    @pronounciation = other.match(/\((.+?)\)/)[1]
+    @type = other.match(/\(.+?\) (\w+):/)[1]
+    @example = other.match(/:(.+)$/)[1]
+
     @guid = latest_word.at_css("guid").text
 
     etag Digest::MD5.hexdigest(settings.development? ? Time.now.to_s : @guid) 
@@ -45,10 +49,12 @@ end
 # == Returns:
 # HTML/CSS edition with etag. This publication changes the greeting depending on the time of day. It is using UTC to determine the greeting.
 #
-get '/sample/' do
+get '/sample/?' do
   @word = "celeste"
   @definition = "heavenly, sky-blue"
-  @example = "celeste (seh-leh'-steh) adjective: Te queda mejor el vestido celeste para la fiesta. - The sky-blue dress looks better on you for the party."
+  @pronounciation = "seh-leh'-steh"
+  @type = "adjective"
+  @example = "Te queda mejor el vestido celeste para la fiesta. - The sky-blue dress looks better on you for the party."
   
   # Set the etag to be this content
   haml :word_of_the_day
